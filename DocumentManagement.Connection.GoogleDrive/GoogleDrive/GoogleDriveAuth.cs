c24ab068f109bd7ac6b52a6aa9c9730908369f60
Ренат <hty007@yandex.ru>
2021-02-15 10:56:33 +0300

    Fix pull request

--- DocumentManagement.Connection.GoogleDrive/GoogleDrive/GoogleDriveAuth.cs
+++ DocumentManagement.Connection.GoogleDrive/GoogleDrive/GoogleDriveAuth.cs
@@ -9,7 +9,6 @@ namespace MRS.DocumentManagement.Connection.GoogleDrive
         public static readonly string AUTH_URI = "https://accounts.google.com/o/oauth2/auth";
         public static readonly string AUTH_PROVIDER = "https://www.googleapis.com/oauth2/v1/certs";
         public static readonly string TOKEN_URI = "https://oauth2.googleapis.com/token";
-        public static readonly string PROJECT_ID = "quickstart-1612934694979";
 
         public static readonly string CLIENT_ID = "1827523568-ha5m7ddtvckjqfrmvkpbhdsl478rdkfm.apps.googleusercontent.com";
         public static readonly string CLIENT_SECRET = "fA-2MtecetmXLuGKXROXrCzt";
