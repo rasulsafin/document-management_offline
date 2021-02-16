455d03926374710607652095affc5a561e1be784
Iana Guryeva <yigurieva@briogroup.ru>
2021-02-16 10:32:17 +0300

    Move item methods to item service from conection service

--- DocumentManagement.Interface/Services/IItemService.cs
+++ DocumentManagement.Interface/Services/IItemService.cs
@@ -4,12 +4,51 @@ using MRS.DocumentManagement.Interface.Dtos;
 
 namespace MRS.DocumentManagement.Interface.Services
 {
+    /// <summary>
+    /// Service for managing files/items.
+    /// </summary>
     public interface IItemService
     {
+        /// <summary>
+        /// Updates item.
+        /// </summary>
+        /// <param name="item">Data to update.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         Task<bool> Update(ItemDto item);
+
+        /// <summary>
+        /// Finds item in db.
+        /// </summary>
+        /// <param name="itemID">Id of item to find.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         Task<ItemDto> Find(ID<ItemDto> itemID);
 
+        /// <summary>
+        /// Gets list of items that belongs to that project.
+        /// </summary>
+        /// <param name="projectID">Project's id.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         Task<IEnumerable<ItemDto>> GetItems(ID<ProjectDto> projectID);
+
+        /// <summary>
+        /// Gets list of items that belongs to that objective.
+        /// </summary>
+        /// <param name="objectiveID">Objective's id.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         Task<IEnumerable<ItemDto>> GetItems(ID<ObjectiveDto> objectiveID);
+
+        /// <summary>
+        /// Download files from remote connection to local storage.
+        /// </summary>
+        /// <param name="itemIds">List of items' id from database.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        Task<IEnumerable<ItemDto>> DownloadItems(IEnumerable<ID<ItemDto>> itemIds);
+
+        /// <summary>
+        /// Delete items from remote connection.
+        /// </summary>
+        /// <param name="itemIds">List of items' id from database.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        Task<bool> DeleteItems(IEnumerable<ID<ItemDto>> itemIds);
     }
 }
