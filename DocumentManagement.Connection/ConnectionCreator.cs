9f814ac047f197f047c59bd58b74ced836388410
Iana Guryeva <yigurieva@briogroup.ru>
2021-03-03 16:45:40 +0300

    Add readonly variables

--- DocumentManagement.Connection/ConnectionCreator.cs
+++ DocumentManagement.Connection/ConnectionCreator.cs
@@ -9,6 +9,9 @@ namespace MRS.DocumentManagement.Connection
 {
     public static class ConnectionCreator
     {
+        private static readonly string METHOD_NAME = "GetConnectionType";
+        private static readonly string SEARCH_PATTERN = "*DocumentManagement.Connection.*.dll";
+
         private static Dictionary<string, Type> connections;
 
         public static IConnection GetConnection(ConnectionTypeDto connectionTypeDto)
@@ -27,15 +30,14 @@ namespace MRS.DocumentManagement.Connection
             connections = new Dictionary<string, Type>();
 
             var list = new List<ConnectionTypeDto>();
-            var listOfTypes = System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
-                        .Where(x => x.Contains("DocumentManagement.Connection."))
+            var listOfTypes = System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, SEARCH_PATTERN)
                         .SelectMany(x => Assembly.Load(AssemblyName.GetAssemblyName(x)).GetTypes())
                         .Where(x => typeof(IConnection).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
 
             foreach (Type type in listOfTypes)
             {
                 object connection = Activator.CreateInstance(type);
-                MethodInfo method = type.GetMethod("GetConnectionType");
+                MethodInfo method = type.GetMethod(METHOD_NAME);
                 object result = method.Invoke(connection, null);
                 ConnectionTypeDto t = result as ConnectionTypeDto;
                 list.Add(t);
