217d9e05af9b2a5fb9cff854f6f010539d749f7e
Anton Sidorov <asidorov@briogroup.ru>
2021-03-17 18:38:38 +0300

    Add path mapping

---
+++ DocumentManagement/Utility/Mapping/Resolvers/ItemExternalDtoRelativePathResolver.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MRS.DocumentManagement.Database.Models;
+using MRS.DocumentManagement.Interface.Dtos;
+
+namespace MRS.DocumentManagement.Utility
+{
+    public class ItemExternalDtoRelativePathResolver : IValueResolver<ItemExternalDto, Item, string>
+    {
+        public ItemExternalDtoRelativePathResolver()
+        {
+        }
+
+        public string Resolve(ItemExternalDto source, Item destination, string destMember, ResolutionContext context)
+        {
+            return PathHelper.GetRelativePath(source.FileName, source.ItemType);
+        }
+    }
+}
