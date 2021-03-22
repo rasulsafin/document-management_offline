b942b546728a52b85c36c9fca436d34d83718628
Iana Guryeva <yigurieva@briogroup.ru>
2021-03-22 16:29:36 +0300

    Fix bim360 pagination

--- DocumentManagement.Connection.BIM360/Forge/Utils/Extensions/PaginationHelper.cs
+++ DocumentManagement.Connection.BIM360/Forge/Utils/Extensions/PaginationHelper.cs
@@ -19,7 +19,7 @@ namespace MRS.DocumentManagement.Connection.Bim360.Forge.Utils.Extensions
             Array.Resize(ref arguments, length + 2);
             arguments[length++] = ITEMS_ON_PAGE;
 
-            for (int i = 0; !all; i++)
+            for (int i = 0; !all; i += ITEMS_ON_PAGE)
             {
                 arguments[length] = i;
                 var response = await connection.SendAsync(
@@ -30,7 +30,7 @@ namespace MRS.DocumentManagement.Connection.Bim360.Forge.Utils.Extensions
                 if (data != null)
                     result.AddRange(data);
                 var meta = response[META_PROPERTY]?.ToObject<Meta>();
-                all = meta == null || meta.Page.Limit * ((meta.Page.Offset / ITEMS_ON_PAGE) + 1) >= meta.RecordCount;
+                all = meta == null || i + ITEMS_ON_PAGE >= meta.RecordCount;
             }
 
             return result;
