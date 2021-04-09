8f94c14f6aff0063c92d0e2d878fe8a4a058a9c9
Iana Guryeva <yigurieva@briogroup.ru>
2021-04-09 09:44:22 +0300

    Fix DI

---
+++ DocumentManagement.Connection.GoogleDrive/GoogleDriveServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using MRS.DocumentManagement.Connection.GoogleDrive;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class GoogleDriveServiceCollectionExtensions
+    {
+        public static IServiceCollection AddYandexDisk(this IServiceCollection services)
+        {
+            services.AddScoped<GoogleConnection>();
+            return services;
+        }
+    }
+}
