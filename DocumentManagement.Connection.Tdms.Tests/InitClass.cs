0e2872ae04e37f5a5b4a4f2f155b40269945b6a7
Ramir Gimaltdinov <rgimaldinov@briogroup.ru>
2021-04-21 10:03:13 +0300

    Fix tdms error

--- DocumentManagement.Connection.Tdms.Tests/InitClass.cs
+++ DocumentManagement.Connection.Tdms.Tests/InitClass.cs
@@ -15,15 +15,16 @@ namespace MRS.DocumentManagement.Connection.Tdms.Tests
         public static void AssemblyInit(TestContext context)
         {
             connection = new TdmsConnection();
+            var connectionMeta = new TdmsConnectionMeta();
             var connectionInfo = new ConnectionInfoExternalDto
             {
-                ConnectionType = connection.GetConnectionType(),
-                AuthFieldValues = new Dictionary<string, string>()
+                ConnectionType = connectionMeta.GetConnectionTypeInfo(),
+                AuthFieldValues = new Dictionary<string, string>
                 {
-                    {Auth.LOGIN, "gureva" },
-                    {Auth.PASSWORD, "123"},
-                    {Auth.DATABASE, "kosmos" },
-                    {Auth.SERVER, @"192.168.100.6\sqlkosmos" },
+                    { Auth.LOGIN, "gureva" },
+                    { Auth.PASSWORD, "123" },
+                    { Auth.DATABASE, "kosmos" },
+                    { Auth.SERVER, @"192.168.100.6\sqlkosmos" },
                 },
             };
 
