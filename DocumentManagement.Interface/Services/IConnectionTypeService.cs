b93facfe02babfce2ee3199b4d2be2453805a634
Iana Guryeva <yigurieva@briogroup.ru>
2021-08-02 08:37:32 +0300

    Update connection types pipeline with new exceptions

--- DocumentManagement.Interface/Services/IConnectionTypeService.cs
+++ DocumentManagement.Interface/Services/IConnectionTypeService.cs
@@ -14,6 +14,8 @@ namespace MRS.DocumentManagement.Interface.Services
         /// </summary>
         /// <param name="typeName">Name of the new connection type.</param>
         /// <returns>ID of added connection type.</returns>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.ArgumentValidationException">Thrown when connection type with type name already exists.</exception>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.DocumentManagementException">Thrown when something else went wrong.</exception>
         Task<ID<ConnectionTypeDto>> Add(string typeName);
 
         /// <summary>
@@ -21,6 +23,8 @@ namespace MRS.DocumentManagement.Interface.Services
         /// </summary>
         /// <param name="id">Type's ID.</param>
         /// <returns>Searching connection type.</returns>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.ANotFoundException">Thrown when connection type with that id does not exists.</exception>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.DocumentManagementException">Thrown when something else went wrong.</exception>
         Task<ConnectionTypeDto> Find(ID<ConnectionTypeDto> id);
 
         /// <summary>
@@ -28,12 +32,15 @@ namespace MRS.DocumentManagement.Interface.Services
         /// </summary>
         /// <param name="name">Type's name.</param>
         /// <returns>Searching connection type.</returns>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.ANotFoundException">Thrown when connection type with that name does not exists.</exception>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.DocumentManagementException">Thrown when something else went wrong.</exception>
         Task<ConnectionTypeDto> Find(string name);
 
         /// <summary>
         /// Get all registered connection types.
         /// </summary>
         /// <returns>Collection of connection types.</returns>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.DocumentManagementException">Thrown when something went wrong.</exception>
         Task<IEnumerable<ConnectionTypeDto>> GetAllConnectionTypes();
 
         /// <summary>
@@ -41,12 +48,15 @@ namespace MRS.DocumentManagement.Interface.Services
         /// </summary>
         /// <param name="id">ID of the type to remove.</param>
         /// <returns>Removing result.</returns>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.ANotFoundException">Thrown when connection type with that id does not exists.</exception>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.DocumentManagementException">Thrown when something else went wrong.</exception>
         Task<bool> Remove(ID<ConnectionTypeDto> id);
 
         /// <summary>
         /// Register all existing Connection Types. Use by admin once at the start OR when new connection types are added.
         /// </summary>
         /// <returns>Result of registration.</returns>
+        /// <exception cref="MRS.DocumentManagement.Interface.Exceptions.DocumentManagementException">Thrown when something went wrong.</exception>
         Task<bool> RegisterAll();
 
     }
