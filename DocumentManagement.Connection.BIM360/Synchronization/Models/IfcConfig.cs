954af24cb9b01fc7d57a50198756ce0a67be708b
Ramir Gimaltdinov <rgimaltdinov@briogroup.ru>
2021-08-02 17:36:46 +0300

    Add config model

---
+++ DocumentManagement.Connection.BIM360/Synchronization/Models/IfcConfig.cs
@@ -0,0 +1,25 @@
+using System.Numerics;
+using System.Runtime.Serialization;
+
+namespace MRS.DocumentManagement.Connection.Bim360.Synchronization.Models
+{
+    [DataContract]
+    internal class IfcConfig
+    {
+        [DataMember]
+        public LinkedModel[] LinkedModels { get; set; }
+
+        [DataContract]
+        public class LinkedModel
+        {
+            [DataMember]
+            public string Urn { get; set; }
+
+            [DataMember]
+            public int Version { get; set; }
+
+            [DataMember]
+            public Vector3 Offset { get; set; }
+        }
+    }
+}
