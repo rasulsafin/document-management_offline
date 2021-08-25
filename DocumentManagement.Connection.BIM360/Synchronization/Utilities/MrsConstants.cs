ba8e6b687851cee1a5e139ed7813cd8c1da3af22
Ramir Gimaltdinov <rgimaltdinov@briogroup.ru>
2021-08-25 12:11:10 +0300

    Fix titles of df

--- DocumentManagement.Connection.BIM360/Synchronization/Utilities/MrsConstants.cs
+++ DocumentManagement.Connection.BIM360/Synchronization/Utilities/MrsConstants.cs
@@ -5,7 +5,7 @@ namespace MRS.DocumentManagement.Connection.Bim360.Synchronization.Utilities
         public static readonly string ISSUE_TYPE_NAME = "Issue";
         public static readonly string LOCATION_DETAILS_FIELD_NAME = "Location Details";
         public static readonly string RESPONSE_FIELD_NAME = "Response";
-        public static readonly string TYPE_FIELD_NAME = "Response";
-        public static readonly string ROOT_CAUSE_FIELD_NAME = "Response";
+        public static readonly string TYPE_FIELD_NAME = "Type";
+        public static readonly string ROOT_CAUSE_FIELD_NAME = "Root Cause";
     }
 }
