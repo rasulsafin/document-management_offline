44017bf0144ea63a138e2b3032e8c1c1a2b5d94e
mzaripov <mzaripov@briogroup.ru>
2021-09-21 14:52:19 +0300

    Fix location model

--- DocumentManagement.Connection.BIM360/Forge/Models/Bim360/Location.cs
+++ DocumentManagement.Connection.BIM360/Forge/Models/Bim360/Location.cs
@@ -4,43 +4,33 @@ using MRS.DocumentManagement.Connection.Bim360.Forge.Models.DataManagement;
 namespace MRS.DocumentManagement.Connection.Bim360.Forge.Models
 {
     [DataContract]
-    public class Location : Object<Location.LocationAttributes, object>
+    public class Location
     {
-        public override string Type
-        {
-            get => Constants.LOCATION_TYPE;
-            set { }
-        }
+        [DataMember(Name = "id")]
+        public string ID { get; set; }
 
-        [DataContract]
-        public class LocationAttributes : AAttributes
-        {
-            [DataMember(Name = "id")]
-            public string Id { get; set; }
+        [DataMember(Name = "parentId")]
+        public object ParentID { get; set; }
 
-            [DataMember(Name = "parentId")]
-            public object ParentID { get; set; }
+        [DataMember(Name = "type")]
+        public string Type { get; set; }
 
-            [DataMember(Name = "type")]
-            public string Type { get; set; }
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
 
-            [DataMember(Name = "name")]
-            public string Name { get; set; }
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
 
-            [DataMember(Name = "description")]
-            public string Description { get; set; }
+        [DataMember(Name = "barcode")]
+        public object BarCode { get; set; }
 
-            [DataMember(Name = "barcode")]
-            public object BarCode { get; set; }
+        [DataMember(Name = "order")]
+        public int Order { get; set; }
 
-            [DataMember(Name = "order")]
-            public int Order { get; set; }
+        [DataMember(Name = "documentCount")]
+        public int DocumentCount { get; set; }
 
-            [DataMember(Name = "documentCount")]
-            public int DocumentCount { get; set; }
-
-            [DataMember(Name = "areaDefined")]
-            public bool AreaDefined { get; set; }
-        }
+        [DataMember(Name = "areaDefined")]
+        public bool AreaDefined { get; set; }
     }
 }
