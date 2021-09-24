475a5b8e6078cc2215f5ff398da7f2377017bddc
mzaripov <mzaripov@briogroup.ru>
2021-09-24 12:22:15 +0300

    Fix "Project" location

--- DocumentManagement.Connection.BIM360/Forge/Services/LocationService.cs
+++ DocumentManagement.Connection.BIM360/Forge/Services/LocationService.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MRS.DocumentManagement.Connection.Bim360.Forge.Models;
-using MRS.DocumentManagement.Connection.Bim360.Forge.Models.Bim360;
 using MRS.DocumentManagement.Connection.Bim360.Forge.Utils;
-using MRS.DocumentManagement.Connection.Bim360.Forge.Utils.Extensions;
-using MRS.DocumentManagement.Connection.Bim360.Forge.Utils.Pagination;
 using MRS.DocumentManagement.Connection.Bim360.Properties;
 using static MRS.DocumentManagement.Connection.Bim360.Forge.Constants;
 
@@ -25,7 +22,11 @@ namespace MRS.DocumentManagement.Connection.Bim360.Forge.Services
                 Resources.GetLocationMethod,
                 containerID,
                 treeID);
-            return response[RESULTS_PROPERTY]?.ToObject<List<Location>>();
+
+            var list = response[RESULTS_PROPERTY]?.ToObject<List<Location>>();
+            list.RemoveAt(0);
+
+            return list;
         }
     }
 }
