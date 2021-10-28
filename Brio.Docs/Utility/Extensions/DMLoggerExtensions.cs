a330d7087b50fa1ec8ef1009d50d7a1024a9a9ab
Ramir Gimaltdinov <rgimaltdinov@briogroup.ru>
2021-10-28 17:08:59 +0300

    Refactor connection creator

--- Brio.Docs/Utility/Extensions/DMLoggerExtensions.cs
+++ Brio.Docs/Utility/Extensions/DMLoggerExtensions.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Brio.Docs.Database.Models;
 using Brio.Docs.Integration;
 using Brio.Docs.Integration.Dtos;
@@ -10,23 +9,27 @@ namespace Brio.Docs.Utility.Extensions
     public static class DMLoggerExtensions
     {
         public static LoggerConfiguration DestructureByIgnoringSensitiveDMInfo(this LoggerConfiguration configuration)
+            => configuration.Destructure.ByIgnoringProperties<User>(x => x.PasswordHash, x => x.PasswordSalt)
+               .Destructure.ByIgnoringProperties<AuthFieldValue>(x => x.Value)
+               .Destructure.ByIgnoringProperties<ConnectionTypeExternalDto>(x => x.AppProperties)
+               .Destructure.ByIgnoringProperties<ConnectionInfoExternalDto>(x => x.AuthFieldValues)
+               .DestructureByIgnoringSensitiveExternalInfo();
+
+        private static LoggerConfiguration DestructureByIgnoringSensitiveExternalInfo(
+            this LoggerConfiguration configuration)
         {
-            configuration = configuration.Destructure.ByIgnoringProperties<User>(
-                x => x.PasswordHash,
-                x => x.PasswordSalt);
-            configuration = configuration.Destructure.ByIgnoringProperties<AuthFieldValue>(x => x.Value);
-            configuration =
-                configuration.Destructure.ByIgnoringProperties<ConnectionTypeExternalDto>(x => x.AppProperties);
-            configuration =
-                configuration.Destructure.ByIgnoringProperties<ConnectionInfoExternalDto>(x => x.AuthFieldValues);
-            configuration = configuration.DestructureByIgnoringSensitiveExternalInfo();
+            var type = typeof(LoggerConfigurationIgnoreExtensions);
+            var method = type.GetMethod(nameof(LoggerConfigurationIgnoreExtensions.ByIgnoringProperties));
+
+            foreach (var expression in ConnectionCreator.GetPropertiesForIgnoringByLogging())
+            {
+                var generic = method!.MakeGenericMethod(expression.SourceType);
+                configuration = (LoggerConfiguration)generic.Invoke(
+                    null,
+                    new object[] { configuration!.Destructure, expression.Expression });
+            }
+
             return configuration;
         }
-
-        private static LoggerConfiguration DestructureByIgnoringSensitiveExternalInfo(this LoggerConfiguration configuration)
-            => ConnectionCreator.GetLoggerMethods()
-               .Aggregate(
-                    configuration,
-                    (aggregated, method) => (LoggerConfiguration)method.Invoke(null, new object[] { aggregated }));
     }
 }
