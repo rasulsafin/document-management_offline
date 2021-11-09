3a678a8c5e21d7b9ff514047312ae55b0e307c5e
Ramir Gimaltdinov <rgimaltdinov@briogroup.ru>
2021-11-09 15:23:24 +0300

    Fix no access exception

--- Brio.Docs.Connections.BIM360/Extensions/DependencyInjection/Bim360ServiceCollectionExtensions.cs
+++ Brio.Docs.Connections.BIM360/Extensions/DependencyInjection/Bim360ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Brio.Docs.Connections.Bim360;
+using Brio.Docs.Connections.Bim360.Interfaces;
 using Brio.Docs.Connections.Bim360.Utilities;
 using Brio.Docs.Connections.Bim360.Utilities.Snapshot;
 
@@ -11,11 +12,14 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddForge();
             services.AddScoped<Bim360Connection>();
             services.AddScoped<EnumerationTypeCreator>();
-            services.AddScoped<TypeSubtypeEnumCreator>();
-            services.AddScoped<RootCauseEnumCreator>();
-            services.AddScoped<LocationEnumCreator>();
-            services.AddScoped<AssignToEnumCreator>();
-            services.AddScoped<StatusEnumCreator>();
+
+            services
+               .AddEnumCreator<TypeSubtypeEnumCreator, IssueTypeSnapshot>()
+               .AddEnumCreator<RootCauseEnumCreator, RootCauseSnapshot>()
+               .AddEnumCreator<LocationEnumCreator, LocationSnapshot>()
+               .AddEnumCreator<AssignToEnumCreator, AssignToVariant>()
+               .AddEnumCreator<StatusEnumCreator, StatusSnapshot>();
+
             services.AddScoped<SnapshotFiller>();
             services.AddScoped<Bim360Snapshot>();
             services.AddScoped<Bim360Storage>();
@@ -25,5 +29,11 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddBim360Synchronization();
             return services;
         }
+
+        private static IServiceCollection AddEnumCreator<TCreator, TSnapshot>(this IServiceCollection services)
+            where TCreator : class, IEnumIdentification<TSnapshot>
+            => services
+               .AddScoped<TCreator>()
+               .AddScoped<IEnumIdentification<TSnapshot>, TCreator>(x => x.GetService<TCreator>());
     }
 }
