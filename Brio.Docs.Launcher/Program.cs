70bdbd88c2f9a0771607a91feaa5f2fa40774fa9
Ramir Gimaltdinov <rgimaltdinov@briogroup.ru>
2021-12-15 11:37:01 +0300

    Kill all processes if directories are not equal

--- Brio.Docs.Launcher/Program.cs
+++ Brio.Docs.Launcher/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using CommandLine;
 
 namespace Brio.Docs.Launcher
@@ -10,8 +12,27 @@ namespace Brio.Docs.Launcher
         {
             var mutName = "Brio.Docs.Launcher";
             using var mutex = new System.Threading.Mutex(true, mutName, out bool createdNew);
+
             if (!createdNew)
-                Environment.Exit(0);
+            {
+                var currentProcess = Process.GetCurrentProcess();
+                var processes = Process.GetProcessesByName(currentProcess.ProcessName);
+                var needToExit = false;
+
+                foreach (var process in processes.Where(x => x.Id != currentProcess.Id))
+                {
+                    if (!string.Equals(currentProcess.MainModule?.FileName, process.MainModule?.FileName))
+                        process.Kill(true);
+                    else
+                        needToExit = true;
+                }
+
+                if (needToExit)
+                {
+                    Console.WriteLine("Brio.Docs.Launcher instance is already running, exiting.");
+                    return;
+                }
+            }
 
             using var app = new App();
             var options = new AppOptions();
