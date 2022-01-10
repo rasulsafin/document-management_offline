c6ab3c5de609187028cd0deab98087a1fff90043
Ramir Gimaltdinov <rgimaltdinov@briogroup.ru>
2022-01-10 15:47:13 +0300

    Add test to add to remote without location

--- Brio.Docs.IntegrationTests/Tests/Synchronization/SynchronizerObjectiveLocationTests.cs
+++ Brio.Docs.IntegrationTests/Tests/Synchronization/SynchronizerObjectiveLocationTests.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using AutoMapper;
+using Brio.Docs.Connections.Bim360.Extensions;
+using Brio.Docs.Database.Extensions;
+using Brio.Docs.Database.Models;
 using Brio.Docs.Integration.Dtos;
 using Brio.Docs.Integration.Interfaces;
 using Brio.Docs.Synchronization;
 using Brio.Docs.Synchronization.Models;
 using Brio.Docs.Tests.Utility;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -16,11 +20,13 @@ namespace Brio.Docs.Tests.Synchronization
     [TestClass]
     public class SynchronizerObjectiveLocationTests : IDisposable
     {
+        private Mock<IConnection> connection;
         private SharedDatabaseFixture fixture;
-        private IMapper mapper;
+        private Mock<ISynchronizer<ObjectiveExternalDto>> objectiveSynchronizer;
+        private (Project local, Project synchronized, ProjectExternalDto remote) projects;
+        private ObjectiveExternalDto remote;
         private ServiceProvider serviceProvider;
         private Synchronizer synchronizer;
-        private Mock<IConnection> connection;
 
         [TestInitialize]
         public async Task Setup()
@@ -28,25 +34,48 @@ namespace Brio.Docs.Tests.Synchronization
             fixture = SynchronizerTestsHelper.CreateFixture();
             serviceProvider = SynchronizerTestsHelper.CreateServiceProvider(fixture.Context);
             synchronizer = serviceProvider.GetService<Synchronizer>();
-            mapper = serviceProvider.GetService<IMapper>();
 
             connection = new Mock<IConnection>();
             var projectSynchronizer = SynchronizerTestsHelper.CreateSynchronizerStub<ProjectExternalDto>();
-            var objectiveSynchronizer = SynchronizerTestsHelper.CreateSynchronizerStub<ObjectiveExternalDto>();
+            objectiveSynchronizer =
+                SynchronizerTestsHelper.CreateSynchronizerStub<ObjectiveExternalDto>(x => remote = x);
             var context = SynchronizerTestsHelper.CreateConnectionContextStub(projectSynchronizer.Object, objectiveSynchronizer.Object);
             connection.Setup(x => x.GetContext(It.IsAny<ConnectionInfoExternalDto>())).ReturnsAsync(context.Object);
-            var projects = await SynchronizerTestsHelper.ArrangeProject(projectSynchronizer, fixture);
+            projects = await SynchronizerTestsHelper.ArrangeProject(projectSynchronizer, fixture);
         }
 
         [TestMethod]
-        public async Task Synchronize_NewLocalObjectiveWithoutLocation_AddNewObjectiveToRemote()
+        public async Task Synchronize_NewLocalObjectiveWithoutLocation_AddObjectiveToRemoteWithoutLocation()
         {
             // Arrange.
+            var objectiveLocal = MockData.DEFAULT_OBJECTIVES[0];
+            objectiveLocal.Project = projects.local;
+            objectiveLocal.ObjectiveType =
+                await fixture.Context.ObjectiveTypes.AsAsyncEnumerable().FirstOrDefaultAsync();
+
+            MockRemoteObjectives(ArraySegment<ObjectiveExternalDto>.Empty);
+            await fixture.Context.Objectives.AddAsync(objectiveLocal);
+            await fixture.Context.SaveChangesAsync();
 
             // Act.
-            await Synchronize();
+            var synchronizationResult = await Synchronize();
+            var local = await fixture.Context.Objectives.Unsynchronized().FirstOrDefaultAsync();
+            var synchronized = await fixture.Context.Objectives.Synchronized().FirstOrDefaultAsync();
 
             // Assert.
+            Assert.AreEqual(0, synchronizationResult.Count);
+            Assert.IsNotNull(local);
+            Assert.IsNotNull(synchronized);
+            Assert.IsNotNull(remote);
+            Assert.AreEqual(null, local.Location);
+            Assert.AreEqual(null, synchronized.Location);
+            Assert.AreEqual(null, remote.Location);
+        }
+
+        [TestMethod]
+        public async Task Synchronize_NewRemoteObjectiveWithoutLocation_AddObjectiveToLocalWithoutLocation()
+        {
+
         }
 
         [TestCleanup]
@@ -57,16 +86,24 @@ namespace Brio.Docs.Tests.Synchronization
         {
             fixture.Dispose();
             serviceProvider.Dispose();
+            GC.SuppressFinalize(this);
         }
 
-        private async Task Synchronize()
-        {
-            await synchronizer.Synchronize(
-                new SynchronizingData(),
+        private void CheckSynchronizerCalls(SynchronizerTestsHelper.SynchronizerCall call, Times times = default)
+            => SynchronizerTestsHelper.CheckSynchronizerCalls(objectiveSynchronizer, call, times);
+
+        private void MockRemoteObjectives(IReadOnlyCollection<ObjectiveExternalDto> array)
+            => SynchronizerTestsHelper.MockGetRemote(objectiveSynchronizer, array, x => x.ExternalID);
+
+        private async Task<ICollection<SynchronizingResult>> Synchronize()
+            => await synchronizer.Synchronize(
+                new SynchronizingData
+                {
+                    User = await fixture.Context.Users.AsAsyncEnumerable().FirstOrDefaultAsync(),
+                },
                 connection.Object,
                 new ConnectionInfoExternalDto(),
                 new Progress<double>(),
                 CancellationToken.None);
-        }
     }
 }
