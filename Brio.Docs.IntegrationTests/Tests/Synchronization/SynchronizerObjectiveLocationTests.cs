08a504172926b41e6ea0921165757d535791f1e1
Ramir Gimaltdinov <rgimaltdinov@briogroup.ru>
2022-01-10 16:50:56 +0300

    Add tests to add location with new objective

--- Brio.Docs.IntegrationTests/Tests/Synchronization/SynchronizerObjectiveLocationTests.cs
+++ Brio.Docs.IntegrationTests/Tests/Synchronization/SynchronizerObjectiveLocationTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Brio.Docs.Common;
+using Brio.Docs.Common.Dtos;
 using Brio.Docs.Database.Extensions;
 using Brio.Docs.Database.Models;
 using Brio.Docs.Integration.Dtos;
@@ -44,6 +45,10 @@ namespace Brio.Docs.Tests.Synchronization
             projects = await SynchronizerTestsHelper.ArrangeProject(projectSynchronizer, fixture);
         }
 
+        [TestCleanup]
+        public void Cleanup()
+            => Dispose();
+
         [TestMethod]
         public async Task Synchronize_NewLocalObjectiveWithoutLocation_AddObjectiveToRemoteWithoutLocation()
         {
@@ -65,8 +70,8 @@ namespace Brio.Docs.Tests.Synchronization
             Assert.AreEqual(0, synchronizationResult.Count);
             Assert.IsNotNull(synchronized);
             Assert.IsNotNull(remote);
-            Assert.AreEqual(null, synchronized.Location);
-            Assert.AreEqual(null, remote.Location);
+            Assert.IsNull(synchronized.Location);
+            Assert.IsNull(remote.Location);
         }
 
         [TestMethod]
@@ -85,19 +90,126 @@ namespace Brio.Docs.Tests.Synchronization
             Assert.AreEqual(0, synchronizationResult.Count);
             Assert.IsNotNull(local);
             Assert.IsNotNull(synchronized);
-            Assert.AreEqual(null, local.Location);
-            Assert.AreEqual(null, synchronized.Location);
+            Assert.IsNull(local.Location);
+            Assert.IsNull(synchronized.Location);
         }
 
-        [TestCleanup]
-        public void Cleanup()
-            => Dispose();
+        [TestMethod]
+        public async Task Synchronize_NewLocalObjectiveWithLocation_AddObjectiveToRemoteWithLocation()
+        {
+            // Arrange.
+            var objectiveLocal = MockData.DEFAULT_OBJECTIVES[0];
+            objectiveLocal.Project = projects.local;
+            objectiveLocal.ObjectiveType =
+                await fixture.Context.ObjectiveTypes.FirstOrDefaultAsync();
+            projects.local.Items ??= new List<Item>();
+            var item = MockData.DEFAULT_ITEMS[0];
+            projects.local.Items.Add(item);
+            objectiveLocal.Location = new Location
+            {
+                PositionX = 1.29,
+                PositionY = -222.5,
+                PositionZ = 0.0001,
+                CameraPositionX = 0.000,
+                CameraPositionY = 0.111,
+                CameraPositionZ = 0.2323,
+                Guid = Guid.NewGuid().ToString(),
+                Item = item,
+            };
+
+            MockRemoteObjectives(ArraySegment<ObjectiveExternalDto>.Empty);
+            await fixture.Context.Objectives.AddAsync(objectiveLocal);
+            await fixture.Context.SaveChangesAsync();
+
+            // Act.
+            var synchronizationResult = await Synchronize();
+            var synchronized = await fixture.Context.Objectives.Synchronized().FirstOrDefaultAsync();
+
+            // Assert.
+            Assert.AreEqual(0, synchronizationResult.Count);
+            Assert.IsNotNull(synchronized);
+            Assert.IsNotNull(remote);
+            Assert.IsNotNull(synchronized.Location);
+            Assert.IsNotNull(remote.Location);
+            AssertLocation(objectiveLocal.Location, remote.Location);
+            AssertLocation(objectiveLocal.Location, synchronized.Location);
+        }
+
+        [TestMethod]
+        public async Task Synchronize_NewRemoteObjectiveWithLocation_AddObjectiveToLocalWithLocation()
+        {
+            // Arrange.
+            ObjectiveExternalDto objectiveRemote = await GetDummyRemoteObjective();
+            projects.local.Items ??= new List<Item>();
+            var item = MockData.DEFAULT_ITEMS[0];
+            projects.local.Items.Add(item);
+            objectiveRemote.Location = new LocationExternalDto
+            {
+                Location = (1.29, -222.5, 0.0001),
+                CameraPosition = (0.000, 0.111, 0.2323),
+                Guid = Guid.NewGuid().ToString(),
+                Item = new ItemExternalDto
+                {
+                    ExternalID = item.ExternalID,
+                    FileName = item.Name,
+                },
+            };
+            await fixture.Context.SaveChangesAsync();
+            MockRemoteObjectives(new[] { objectiveRemote });
+
+            // Act.
+            var synchronizationResult = await Synchronize();
+            var local = await fixture.Context.Objectives.Unsynchronized().FirstOrDefaultAsync();
+            var synchronized = await fixture.Context.Objectives.Synchronized().FirstOrDefaultAsync();
+
+            // Assert.
+            Assert.AreEqual(0, synchronizationResult.Count);
+            Assert.IsNotNull(local);
+            Assert.IsNotNull(synchronized);
+            Assert.IsNotNull(local.Location);
+            Assert.IsNotNull(synchronized.Location);
+            AssertLocation(objectiveRemote.Location, local.Location);
+            AssertLocation(objectiveRemote.Location, synchronized.Location);
+        }
 
         public void Dispose()
         {
             fixture.Dispose();
             serviceProvider.Dispose();
             GC.SuppressFinalize(this);
+        }
+
+        private void AssertLocation(Location expected, Location actual)
+        {
+            Assert.AreEqual(expected.Guid, actual.Guid);
+            Assert.AreEqual(expected.PositionX, actual.PositionX);
+            Assert.AreEqual(expected.PositionY, actual.PositionY);
+            Assert.AreEqual(expected.PositionZ, actual.PositionZ);
+            Assert.AreEqual(expected.CameraPositionX, actual.CameraPositionX);
+            Assert.AreEqual(expected.CameraPositionY, actual.CameraPositionY);
+            Assert.AreEqual(expected.CameraPositionZ, actual.CameraPositionZ);
+        }
+
+        private void AssertLocation(Location expected, LocationExternalDto actual)
+        {
+            Assert.AreEqual(expected.Guid, actual.Guid);
+            Assert.AreEqual(expected.PositionX, actual.Location.x);
+            Assert.AreEqual(expected.PositionY, actual.Location.y);
+            Assert.AreEqual(expected.PositionZ, actual.Location.z);
+            Assert.AreEqual(expected.CameraPositionX, actual.CameraPosition.x);
+            Assert.AreEqual(expected.CameraPositionY, actual.CameraPosition.y);
+            Assert.AreEqual(expected.CameraPositionZ, actual.CameraPosition.z);
+        }
+
+        private void AssertLocation(LocationExternalDto expected, Location actual)
+        {
+            Assert.AreEqual(expected.Guid, actual.Guid);
+            Assert.AreEqual(expected.Location.x, actual.PositionX);
+            Assert.AreEqual(expected.Location.y, actual.PositionY);
+            Assert.AreEqual(expected.Location.z, actual.PositionZ);
+            Assert.AreEqual(expected.CameraPosition.x, actual.CameraPositionX);
+            Assert.AreEqual(expected.CameraPosition.y, actual.CameraPositionY);
+            Assert.AreEqual(expected.CameraPosition.z, actual.CameraPositionZ);
         }
 
         private void CheckSynchronizerCalls(SynchronizerTestsHelper.SynchronizerCall call, Times times = default)
