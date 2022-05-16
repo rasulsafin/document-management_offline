f340f5e5cd06fbeeac73200bad8c2f0bd77831ee
mzaripov <mzaripov@briogroup.ru>
2022-05-16 15:21:20 +0300

    Fix images size in footer and header

--- Brio.Docs.Utils/Report/ReportCreator.cs
+++ Brio.Docs.Utils/Report/ReportCreator.cs
@@ -41,6 +41,8 @@ namespace Brio.Docs.Utils.ReportCreator
 
         private static readonly string ROOT = "Report";
 
+        private static readonly string LOGO_IMAGE_ID = "rId1";
+
         internal static MainDocumentPart MainPart { get; set; }
 
         public void CreateReport(XDocument xml, XDocument footer, XDocument header, string outputDocument)
@@ -127,84 +129,16 @@ namespace Brio.Docs.Utils.ReportCreator
                     sectPr.PrependChild<FooterReference>(new FooterReference() { Id = footerId, Type = new EnumValue<HeaderFooterValues>(HeaderFooterValues.Default) });
                 }
 
-                var bigLogoImagePart = headerPart.AddImagePart(ImagePartType.Jpeg);
+                var bigLogoImagePart = headerPart.AddImagePart(ImagePartType.Jpeg, LOGO_IMAGE_ID);
                 bigLogoImagePart.FeedData(logoBigStream);
-                var bigLogoImagePartId = headerPart.GetIdOfPart(bigLogoImagePart);
-                var bigLogoImage = Image.FromStream(logoBigStream);
-                InsertImage(headerPart.Header, bigLogoImagePartId, bigLogoImage);
 
-                var smallLogoImagePart = footerPart.AddImagePart(ImagePartType.Png);
+                var smallLogoImagePart = footerPart.AddImagePart(ImagePartType.Png, LOGO_IMAGE_ID);
                 smallLogoImagePart.FeedData(logoSmallStream);
-                var smallLogoImagePartId = footerPart.GetIdOfPart(smallLogoImagePart);
-                var smallLogoImage = Image.FromStream(logoSmallStream);
-                InsertImage(footerPart.Footer, smallLogoImagePartId, smallLogoImage);
 
                 logoBigStream.Close();
                 logoSmallStream.Close();
 
                 mainPart.Document.Save();
-            }
-        }
-
-        private void InsertImage(OpenXmlElement parent, string relationId, Image image)
-        {
-            if (!string.IsNullOrEmpty(relationId))
-            {
-                Int64Value width = image.Size.Width * 9525;
-                Int64Value height = image.Size.Height * 9525;
-
-                var draw = new Drawing(
-                    new DW.Inline(
-                        new DW.Extent() { Cx = width, Cy = height },
-                        new DW.EffectExtent()
-                        {
-                            LeftEdge = 0L,
-                            TopEdge = 0L,
-                            RightEdge = 0L,
-                            BottomEdge = 0L,
-                        },
-                        new DW.DocProperties()
-                        {
-                            Id = (UInt32Value)1U,
-                            Name = "my image name",
-                        },
-                        new DW.NonVisualGraphicFrameDrawingProperties(new A.GraphicFrameLocks() { NoChangeAspect = true }),
-                        new A.Graphic(
-                            new A.GraphicData(
-                                new PIC.Picture(
-                                    new PIC.NonVisualPictureProperties(
-                                        new PIC.NonVisualDrawingProperties()
-                                        {
-                                            Id = (UInt32Value)0U,
-                                            Name = relationId,
-                                        },
-                                        new PIC.NonVisualPictureDrawingProperties()),
-                                    new PIC.BlipFill(
-                                        new A.Blip(
-                                            new A.BlipExtensionList(
-                                                new A.BlipExtension() { Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}" }))
-                                        {
-                                            Embed = relationId,
-                                            CompressionState = A.BlipCompressionValues.Print,
-                                        },
-                                        new A.Stretch(
-                                        new A.FillRectangle())),
-                                    new PIC.ShapeProperties(
-                                        new A.Transform2D(
-                                            new A.Offset() { X = 0L, Y = 0L },
-                                            new A.Extents() { Cx = width, Cy = height }),
-                                        new A.PresetGeometry(new A.AdjustValueList()) { Preset = A.ShapeTypeValues.Rectangle })))
-                            { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
-                                                            )
-                    {
-                        DistanceFromTop = (UInt32Value)0U,
-                        DistanceFromBottom = (UInt32Value)0U,
-                        DistanceFromLeft = (UInt32Value)0U,
-                        DistanceFromRight = (UInt32Value)0U,
-                        EditId = "50D07946",
-                    });
-
-                parent.Append(new Paragraph(new Run(draw)));
             }
         }
 
